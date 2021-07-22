6f42b55faad0172328849a09532f9756f89af64c
Juanjo <juanjoserequenalama@gmail.com>
2021-07-22 22:52:16 +0200

    tests are done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import React, {Component} from 'react';
+import logo from './coverwalletlogo.png';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends Component{
+
+  constructor(props){
+    super(props);
+    this.state = {
+      n:"",
+      res:"",
+    }
+  }
+  updateFibonacci(){
+  var num = this.state.n;
+    if(!(num<0 ||num==="" || num===null || isNaN(num))){
+      console.log(num)
+      this.setState({res:this.fibonacci(num)});
+
+    }else{
+      //alert("Must input a valid number");
+      this.setState({res:""});
+    }
+
+  }
+
+  fibonacci(n){
+    var i = 1, j = 0;
+
+    while (n >=1){
+      j=i+j;
+      i=j-i;
+      n--;
+    }
+    return j;
+  }
+
+  render(){
+    
+    return(
+      <div className="App" >
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <input data-testid="input" className="N-input" type="number" min='0' placeholder="Enter a number" onChange={e=>this.setState({ n: e.target.value })}></input>
+          <button data-testid="button" className="Calc-btn" onClick={() => this.updateFibonacci() }>Calculate</button>
+          <div data-testid="result" className="Res-div"id="result"> {this.state.res}</div>
+        </header>
     </div>
-  );
+
+    )
+  }
 }
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,75 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+describe('Tests for different fibonacci solutions', () => {
+  test('Fibonacci(n=10)', () => {
+    const { getByTestId } = render(<App />);
+    const btnEl = getByTestId("button");
+    const inputEl = getByTestId("input");
+    const resEl = getByTestId("result");
+  
+    fireEvent.change(inputEl, {
+      target: {
+        value:"10"
+      }
+    })
+    fireEvent.click(btnEl);
+    
+    expect(resEl.textContent.trim()).toBe("55")
+  
+  
+  });
+  test('Fibonacci(n=1000000)', () => {
+    const { getByTestId } = render(<App />);
+    const btnEl = getByTestId("button");
+    const inputEl = getByTestId("input");
+    const resEl = getByTestId("result");
+    fireEvent.change(inputEl, {
+      target: {
+        value:"1000000"
+      }
+    })
+    fireEvent.click(btnEl);
+    
+    expect(resEl.textContent.trim()).toBe("NaN")
+  
+  
+  });
+  test('Fibonacci(empty string)', () => {
+    const { getByTestId } = render(<App />);
+    const btnEl = getByTestId("button");
+    const inputEl = getByTestId("input");
+    const resEl = getByTestId("result");
+    fireEvent.change(inputEl, {
+      target: {
+        value:""
+      }
+    })
+    fireEvent.click(btnEl);
+    
+    expect(resEl.textContent.trim()).toBe("")
+  
+  
+  });
+  test('Fibonacci(-1)', () => {
+    const { getByTestId } = render(<App />);
+    const btnEl = getByTestId("button");
+    const inputEl = getByTestId("input");
+    const resEl = getByTestId("result");
+    fireEvent.change(inputEl, {
+      target: {
+        value:"-1"
+      }
+    })
+    fireEvent.click(btnEl);
+    
+    expect(resEl.textContent.trim()).toBe("")
+  
+  });
+
+
+
+
+
+})
+
